b240b3381ee3e02055b18cfb3773a5120bdb525b
João lucas <jlvatejo123@gmail.com>
2023-11-23 09:41:39 -0300

    Creating header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,31 @@
+import {Link} from 'react-router-dom'
+import {FiUser, FiLogIn} from 'react-icons/fi'
 export function Header(){
+    const signed = false;
+    const loadingAuth = false;
+    
     return(
-      <div>
-  
-        <h1>COMPONENTE HEADER</h1>
-
+      <div className="w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4">
+        <header className="flex w-full max-w-7xl items-center justify-between px-4 mx-auto">
+            <Link to="/">
+                <h1>Hope Child</h1>
+            </Link>
+           
+            {!loadingAuth && signed &&(
+                 <Link to='/dashboard'>
+                    <div className="border-2 rounded-full p-1 border-gray-900">
+                    <FiUser size={24} color="#000"/>
+                    </div>
+             </Link>
+            )}
+            {!loadingAuth && !signed &&(
+                 <Link to='/login'>
+                <div className="border-2 rounded-full p-1 border-gray-900">
+                    <FiLogIn size={24} color="#000"/>
+                </div>
+             </Link>
+            )}
+        </header>
       </div>
     )
   }
